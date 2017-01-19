4d1a2baa48d315bc149e719ec8c8fe518aef1f1c
Athul <athul.satheesh166@gmail.com>
2017-01-19 10:02:25 +0000

    first commit

---
+++ js/sample_5.js
@@ -0,0 +1,70 @@
+var fs    =    require('fs');
+var util  =   require('util');
+var file    =    "Production-Department_of_Agriculture_and_Cooperation_1.csv";
+fs.readFile(file,'utf-8',function(error,data)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    else
+    {
+        var mycsv=data;
+        function csvJSON(mycsv) {
+            var lines=mycsv.split("\n");
+            var result = [];
+            var headers = lines[0].split(",");
+            for(var i=1; i<lines.length; i++)
+             {
+                var obj = {};
+
+                var row = lines[i],
+                queryIdx = 0,
+                startValueIdx = 0,
+                idx = 0;
+
+                if (row.trim() === '')
+                 {
+                     continue;
+                  }
+
+                while (idx < row.length)
+                 {
+                    var c = row[idx];
+
+                    if (c === '"')
+                    {
+                        do { c = row[++idx]; } while (c !== '"' && idx < row.length - 1);
+                    }
+
+                    if (c === ',' || idx === row.length - 1)
+                     {
+                        var value = row.substr(startValueIdx, idx - startValueIdx).trim();                       
+                        var key = headers[queryIdx++];
+                        obj[key] = value;
+                        startValueIdx = idx + 1;
+                     }
+                    ++idx;
+                }
+                if(obj['Particulars'].includes("Agricultural Production Oilseeds") && obj[' 3-2013']!=="NA")
+                {
+                      var obj_1={};
+                      obj_1['Particulars']=obj['Particulars'];
+                      obj_1['Production']=obj[' 3-2013'];
+                      result.push(obj_1);
+                }
+            }
+            return result.sort(function(a, b) {
+            return parseFloat(b['Production']) - parseFloat(a['Production']);
+            });;
+        }
+        //console.log(csvJSON(mycsv));
+       fs.writeFile("D3input_1.json", JSON.stringify(csvJSON(mycsv)),'utf-8', function(err) {
+          if(err) {
+              return console.log(err);
+          }
+
+          console.log("The file was saved!");
+      });
+    }
+});
